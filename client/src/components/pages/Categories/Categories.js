953571cb0fc34c11e8203bbb6ac99737b117d778
miriacode <miriamacunaenciso@gmail.com>
2022-05-30 19:06:39 -0500

    Favorite categories section updated

--- client/src/components/pages/Categories/Categories.js
+++ client/src/components/pages/Categories/Categories.js
@@ -5,15 +5,43 @@ import { Link } from "react-router-dom";
 
 const Categories = ({userId}) => {
 
-    // const [allCategoriesList, setAllCategoriesList] = useState([]);
     const [incomeCategoryList, setIncomeCategoryList] = useState([]);
     const [expensesCategoryList, setExpensesCategoryList] = useState([]);
+
+    const[favoriteCategoryList, setFavoriteCategoryList] = useState([]);
     
     useEffect(() =>{
         axios.get("http://localhost:8000/api/categories/find/"+userId,{withCredentials: true})
             .then(res => {
                 setIncomeCategoryList(res.data.filter(category=>category.type==="income"))
                 setExpensesCategoryList(res.data.filter(category=>category.type==="expense"))
+            })
+            .catch(error => console.log(error));
+    }, [userId])
+
+    useEffect(() =>{
+        axios.get("http://localhost:8000/api/transactions/find/"+userId,{withCredentials: true})
+            .then(res => {
+               let allTransactionsList = res.data;
+
+               const map = new Map()
+
+                for (let i = 0; i < allTransactionsList.length; i++) {
+                    if (!map.has(allTransactionsList[i].category)) {
+                        map.set(allTransactionsList[i].category, 1)
+                    } else {
+                        let counter = map.get(allTransactionsList[i].category)
+                        counter = counter + 1;
+                        map.set(allTransactionsList[i].category, counter)
+                    }
+                }
+                let newMap = new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
+                let allCategoriesUsed = []
+                for (const key of newMap.keys()) {
+                    allCategoriesUsed.push(key)
+                }
+                allCategoriesUsed.splice(3,)
+                setFavoriteCategoryList(allCategoriesUsed)
             })
             .catch(error => console.log(error));
     }, [userId])
@@ -37,9 +65,7 @@ const Categories = ({userId}) => {
             <Link to="/categories/new">Add Category</Link>
             <p>Favorite Categories</p>
             <ul>
-                <li>3</li>
-                <li>7</li>
-                <li>8</li>
+               {favoriteCategoryList.map(category=><li>{category}</li>)}
             </ul>
 
             <h4>All Categories</h4>
@@ -58,6 +84,8 @@ const Categories = ({userId}) => {
             {incomeCategoryList.length===0?<p>You don't have any expenses categories yet</p>:incomeCategoryList.map((category, index) => (
                 <div className="container border border-dark" key={index}>
                     <p>{category.categoryName}</p>
+                    <Link to={"/categories/update/"+category._id}>Edit</Link> 
+                    <button onClick={() => deleteCategory(category._id)}>Delete</button>
                 </div>
             ))
             }       
