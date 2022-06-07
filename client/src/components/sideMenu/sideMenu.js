09e5e1653487e3f3727710e46a30c0cebcc56f9e
miriacode <miriamacunaenciso@gmail.com>
2022-06-07 16:03:15 -0500

    Adding styles

--- client/src/components/sideMenu/sideMenu.js
+++ client/src/components/sideMenu/sideMenu.js
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
-//CSS
-import './SideMenu.css'
+//Styles
+import styles from './SideMenu.module.scss'
+
+//Material Icons
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 const SideMenu = ({userId, switchTheme, theme}) => {
     const [user, setUser] = useState({});
@@ -10,22 +15,23 @@ const SideMenu = ({userId, switchTheme, theme}) => {
     useEffect(() => {
         axios.get("http://localhost:8000/api/users/"+userId,{withCredentials: true})
             .then(res => {
-                //console.log(res.data)
                 setUser(res.data)
-               
             })
             .catch(error => console.log(error));
     }, [userId,user]);
 
     return (
-        <div className="side-menu">
-            <div>Luna/Sol</div>
-            <button onClick={switchTheme}>
-                Switch to {theme === 'light'? 'Dark' : 'Light'}
-            </button>
-            <h2>{user.firstName} {user.lastName}</h2>
-            {user.profilePictureURL!==undefined?<img src={require("./../../uploads/profilePicture/"+user.profilePictureURL)} alt={`${user.firstName}-${user.lastName}-profile-picture}`}/>:null}
-            <div>Pic</div>
+        <div className={styles.sidemenu}>
+            <div className={styles.sidemenu__theme}>
+                <button onClick={switchTheme} className={styles.sidemenu__button}>
+                {theme === 'light'? <DarkModeIcon className={styles.sidemenu__icon}></DarkModeIcon> : <LightModeIcon className={styles.sidemenu__icon}></LightModeIcon>}
+                </button>
+            </div>
+            <div className={styles.sidemenu__user}>
+                {user.profilePictureURL!==undefined?<img className={styles.sidemenu__userimage} src={require("./../../uploads/profilePicture/"+user.profilePictureURL)} alt={`${user.firstName}-${user.lastName}-profile-picture}`}/>:null}
+                <Link className={styles.sidemenu__username} to="/profile">{user.firstName} {user.lastName}</Link>
+            </div>
+            
         </div>
     )
 }
