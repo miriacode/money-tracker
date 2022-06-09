2b5ae6476a945de56210d67b2f68476d4167a9b8
miriacode <miriamacunaenciso@gmail.com>
2022-06-09 13:06:36 -0500

    Adding styles

--- client/src/components/pages/UpdateProfile/UpdateProfile.js
+++ client/src/components/pages/UpdateProfile/UpdateProfile.js
@@ -2,8 +2,11 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+//Styles
+import styles from "./../Profile/Profile.module.css"
 
-const Profile = ({userId}) => {
+
+const EditProfile = ({userId}) => {
 
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
@@ -11,7 +14,7 @@ const Profile = ({userId}) => {
     const [cellphone, setCellphone] = useState("");
     const [location, setLocation] = useState("");
     const [postalCode, setPostalCode] = useState("");
-    const [profilePictureURL, setProfilePictureURL] = useState("");
+    const [profilePictureURL, setProfilePictureURL] = useState(undefined);
     const [chosenImage, setChosenImage] = useState(null);
     
     const [errors, setErrors] = useState({});
@@ -30,6 +33,7 @@ const Profile = ({userId}) => {
                 setLocation(res.data.location)
                 setPostalCode(res.data.postalCode)
                 setProfilePictureURL(res.data.profilePictureURL)
+                // setProfilePictureURL(res.data.profilePictureURL)
             })
             .catch(error => console.log(error));
     }, [userId])
@@ -69,48 +73,67 @@ const Profile = ({userId}) => {
         setChosenImage(e.target.files[0])
     }
 
+    let inputFileStyles = {
+        
+    }
+
     return (
-        <div>
-            <h2>Profile</h2>
-            {profilePictureURL}
-            <form onSubmit={updateProfile}>
-                    <input type="file" name="profilePicture" onChange={handleFile}/>
+        <div className={styles.page}>
+            <h2 className={styles.page__title}>Profile</h2>
+            <div>
+                <form onSubmit={updateProfile} className={styles.user__form}>
+                    <div className={styles.user__basic}>
+                        <div className={styles.user__left}>
+                            {profilePictureURL!==undefined?<img src={require("./../../../uploads/profilePicture/"+profilePictureURL)} alt={`${firstName}-${lastName}-profile-picture}`}/>:null}
+                            <div className={styles.user__edit}>
+                                <input type="file" name="profilePicture" onChange={handleFile} style={inputFileStyles}/>
+                            </div>
+                        </div>
+                        <span></span>
+                        <div className={styles.user__right}>
+                            <h3>{firstName} {lastName}</h3>
+                        </div> 
+                    </div>
+            
+                       
 
-                <div className="form-group">
-                    <label htmlFor="firstName">First Name:</label>
-                    <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="form-control" />
-                    {errors.firstName? <span className="text-danger">{errors.firstName.message}</span> : null}
-                </div>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="form-control" />
+                        {errors.firstName? <span className="text-danger">{errors.firstName.message}</span> : null}
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="lastName">Last Name:</label>
-                    <input type="text" id="lastName" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} className="form-control" />
-                    {errors.lastName ? <span className="text-danger">{errors.lastName.message}</span> : null}
-                </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input type="text" id="lastName" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} className="form-control" />
+                        {errors.lastName ? <span className="text-danger">{errors.lastName.message}</span> : null}
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="cellphone">Cellphone:</label>
-                    <input type="text" id="cellphone" name="cellphone" value={cellphone} onChange={(e) => setCellphone(e.target.value)} className="form-control" />
-                    {errors.cellphone ? <span className="text-danger">{errors.cellphone.message}</span> : null}
-                </div>
+                    <div className="form-group">
+                        <label htmlFor="cellphone">Cellphone:</label>
+                        <input type="text" id="cellphone" name="cellphone" value={cellphone} onChange={(e) => setCellphone(e.target.value)} className="form-control" />
+                        {errors.cellphone ? <span className="text-danger">{errors.cellphone.message}</span> : null}
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="cellphone">Location:</label>
-                    <input type="text" id="location" name="location" value={location} onChange={(e) => setLocation(e.target.value)} className="form-control" />
-                    {errors.location ? <span className="text-danger">{errors.location.message}</span> : null}
-                </div>
+                    <div className="form-group">
+                        <label htmlFor="cellphone">Location:</label>
+                        <input type="text" id="location" name="location" value={location} onChange={(e) => setLocation(e.target.value)} className="form-control" />
+                        {errors.location ? <span className="text-danger">{errors.location.message}</span> : null}
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="postalCode">Postal Code:</label>
-                    <input type="text" id="postalCode" name="postalCode" value={postalCode} onChange={(e) => setPostalCode(e.target.value)} className="form-control" />
-                    {errors.postalCode ? <span className="text-danger">{errors.postalCode.message}</span> : null}
-                </div>
-                
-                <button type="submit">Submit</button>
-                {/* <input type="submit" value="Update" className="btn btn-success" /> */}
-            </form>
+                    <div className="form-group">
+                        <label htmlFor="postalCode">Postal Code:</label>
+                        <input type="text" id="postalCode" name="postalCode" value={postalCode} onChange={(e) => setPostalCode(e.target.value)} className="form-control" />
+                        {errors.postalCode ? <span className="text-danger">{errors.postalCode.message}</span> : null}
+                    </div>
+                    
+                    <button type="submit">Submit</button>
+                    {/* <input type="submit" value="Update" className="btn btn-success" /> */}
+                </form>
+            </div>
+            
         </div>
     )
 }
 
-export default Profile;
+export default EditProfile;
