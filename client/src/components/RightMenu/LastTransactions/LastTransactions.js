9900ebc0e5e6f92feb297c8b6258507a278c81fd
miriacode <miriamacunaenciso@gmail.com>
2022-06-14 15:17:51 -0500

    Adding styles

--- client/src/components/RightMenu/LastTransactions/LastTransactions.js
+++ client/src/components/RightMenu/LastTransactions/LastTransactions.js
@@ -1,21 +1,64 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 
+//Material UI Icons
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+//Styles
+import styles from "./../LastTransactions/LastTransactions.module.css"
+import { makeStyles } from "@mui/material";
+
+
 const LastTransactions = ({userId}) => {
 
     const [lastTransactions, setLastTransactions] = useState([])
+    const [date, setDate] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:8000/api/transactions/last5/find/"+userId,{withCredentials: true})
-            .then(res => setLastTransactions(res.data))
+            .then(res => {
+                setLastTransactions(res.data)
+                // lastTransactions.forEach(transaction=> {
+                //     let date1 = new Date(transaction.date).toUTCString().split(" ")
+                //     // let date2 = date1
+                //     // let newDate = `${date1[2]} ${date1[1]}, ${date1[3]}`
+                //     // setDate()
+                // })
+                // setLastTransactions(y)
+  
+            })
             .catch(error => console.log(error));
     }, [userId]);
 
     return (
-        <div>
-            <h2>LastTransactions</h2>
-            <ul>
-                {lastTransactions.map((transaction,i)=><li key={i}>{transaction.type} {transaction.title}</li>)}
+        <div className={styles.lastTransactions}>
+
+            <h2 className={styles.lastTransactions__title}>LastTransactions</h2>
+
+            <ul className={styles.lastTransactions__list}>
+
+                {lastTransactions.map((transaction,i)=>
+                <li className={styles.transaction} key={i}>
+                    {transaction.type==="income"?
+
+                    <div className={styles.transaction__icon} style={{backgroundColor:"blue"}}>
+                        <ArrowDropUpIcon style={{color:"red"}}></ArrowDropUpIcon>
+                    </div>:
+
+                    <div className={styles.transaction__icon} style={{backgroundColor:"green"}}>
+                        <ArrowDropDownIcon style={{color:"yellow"}}></ArrowDropDownIcon>
+                    </div>
+                    }
+
+                    <div>
+                        <span className={styles.transaction__title}>{transaction.title}</span>
+                        <span className={styles.transaction__date}>May, 25 2020</span>
+                    </div>
+                    <div>
+                        <span className={styles.transaction__amount}>${transaction.amount}</span>
+                    </div>
+                </li>)}
             </ul>
         </div>
     )
