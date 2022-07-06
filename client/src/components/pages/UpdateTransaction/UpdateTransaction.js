0027e096846b01740e5564bae1c0269d90a0f625
miriacode <miriamacunaenciso@gmail.com>
2022-07-06 18:36:15 -0500

    Adding styles

--- client/src/components/pages/UpdateTransaction/UpdateTransaction.js
+++ client/src/components/pages/UpdateTransaction/UpdateTransaction.js
@@ -2,7 +2,12 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import {useParams, useNavigate} from "react-router-dom";
 
-const UpdateTransaction = ({userId}) => {
+//Styles
+import styles from './UpdateTransaction.module.css'
+
+import RightMenu from '../../RightMenu/RightMenu'
+
+const UpdateTransaction = ({userId, theme}) => {
     
     const {id} = useParams();
 
@@ -61,64 +66,132 @@ const UpdateTransaction = ({userId}) => {
     }
 
     return (
-        <div>
-            <h2>Update Transaction</h2>
-            <div>
-            <form onSubmit={updateTransaction}>
-                <div className="form-check">
-                    <label>
-                    Type:
-                        <select value={type} onChange={(e)=>setType(e.target.value)}>
-                            <option value="expense">Expense</option>
-                            <option value="income">Income</option>
-                        </select>
+        <>
+        <div className={styles.page}>
+            <h2 className={styles.page__title}>Update Transaction</h2>
+            <form onSubmit={updateTransaction} className={styles.transaction}>
+                <div className={styles.transaction__control}>
+                    <label className={styles.transaction__label}>Type:</label>
+                    <select 
+                        className={`${styles.transaction__input} ${styles.transaction__input__select}`}
+                        value={type} 
+                        onChange={(e)=>setType(e.target.value)}>
+                        <option value="expense">Expense</option>
+                        <option value="income">Income</option>
+                     </select>
+                </div>
+
+                <div className={styles.transaction__control}>
+                    <label 
+                        htmlFor="title" 
+                        className={styles.transaction__label}>
+                        Title:
                     </label>
+                    <input 
+                        className={errors.title?
+                                    `${styles.transaction__input} ${styles.transaction__input__text} ${styles.transaction__input__error}`:
+                                    `${styles.transaction__input} ${styles.transaction__input__text}`}
+                        type="text" 
+                        id="title" 
+                        name="title" 
+                        value={title} 
+                        onChange={(e) => {
+                                setTitle(e.target.value)
+                                const {title, ...rest} = errors;
+                                setErrors(rest)}}/>   
                 </div>
+                    {errors.title? <span className={styles.error}>{errors.title.message}</span> : null}
+                <div className={styles.transaction__control__big}>
+                    <label htmlFor="description" className={`${styles.transaction__label}`}>Description:</label>
+                    <textarea
+                        // rows={15}
+                        // cols={5}
+                        className={`${styles.transaction__input} ${styles.transaction__input__textarea}`}
+                        id="description" 
+                        name="description" 
+                        value={description} 
+                        onChange={(e) => setDescription(e.target.value)}></textarea>
+                </div>
+                    {errors.description ? <span className={styles.error}>{errors.description.message}</span> : null}
+                <div className={styles.transaction__control}>
+                    <label htmlFor="amount" className={styles.transaction__label}>Amount</label>$
+                    <input 
+                        className={errors.amount?
+                            `${styles.transaction__input} ${styles.transaction__input__number} ${styles.transaction__input__error}`:
+                            `${styles.transaction__input} ${styles.transaction__input__number}`}
+                        type="number" 
+                        id="amount" 
+                        name="amount"
+                        value={amount} 
+                        // min={0}
+                        onChange={(e) => {
+                            setAmount(e.target.value)
+                            const {amount, ...rest} = errors;
+                            setErrors(rest)}}/> 
+                </div>
+                    {errors.amount ? <span className={styles.error}>{errors.amount.message}</span> : null}
+                <div className={styles.transaction__control}>
+                    <label className={styles.transaction__label}>Category:</label>
+                        <select
+                            className={errors.category?
+                                `${styles.transaction__input} ${styles.transaction__input__select} ${styles.transaction__input__error}`:
+                                `${styles.transaction__input} ${styles.transaction__input__select}`}
+                            value={category} 
+                            onChange={(e) => {
+                                setCategory(e.target.value)
+                                const {category, ...rest} = errors;
+                                setErrors(rest)}}>
+                            <option>------------</option>
+                            {type==="expense"?
+                                categoryList.filter(category=>category.type==="expense")
+                                            .map((category,index)=>(
+                                            <option 
+                                                key={index} 
+                                                value={category.categoryName}>
+                                                    {category.categoryName}
+                                            </option>
+                            )):categoryList.filter(category=>category.type==="income")
+                                            .map((category,index)=>(
+                                            <option 
+                                                key={index} 
+                                                value={category.categoryName}>
+                                                    {category.categoryName}
+                                            </option>
+                            ))}
+                        </select>
+                </div>    
+                    {errors.category ? <span className={styles.error}>{errors.category.message}</span> : null}
 
-                <div className="form-group">
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" />
-                    {errors.title? <span className="text-danger">{errors.title.message}</span> : null}
+                <div className={styles.transaction__control}>
+                    <label className={styles.transaction__label}>Date</label>
+                    <input 
+                        className={theme==="light"?
+                        errors.date?`${styles.transaction__input} ${styles.transaction__input__date} ${styles.transaction__input__error}`: 
+                        `${styles.transaction__input} ${styles.transaction__input__date} `:
+                        errors.date?`${styles.transaction__input} ${styles.transaction__input__date} ${styles.transaction__input__date__dark} ${styles.transaction__input__error}`: 
+                        `${styles.transaction__input} ${styles.transaction__input__date} ${styles.transaction__input__date__dark}`
+                        }
+                        type="date" 
+                        value={date} 
+                        onChange={(e) => {
+                            setDate(e.target.value)
+                            const {date, ...rest} = errors;
+                            setErrors(rest)}}/>
                 </div>
-
-                <div className="form-group">
-                    <label htmlFor="description">Description:</label>
-                    <input type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)} className="form-control" />
-                    {errors.description ? <span className="text-danger">{errors.description.message}</span> : null}
-                </div>
-                <div className="form-group">
-                    <label htmlFor="amount">Amount</label>
-                    $<input type="number" id="amount" name="amount" value={amount} onChange={(e) => setAmount(e.target.value)} className="form-control" />
-                    {errors.amount ? <span className="text-danger">{errors.amount.message}</span> : null}
-                </div>
+                {errors.date ? <span  className={styles.error}>{errors.date.message}</span> : null}
                 
-                <div className="form-check">
-                    <label>
-                    Category:
-                        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-                            <option>-----</option>
-                            {type==="expense"?categoryList.filter(category=>category.type==="expense").map((category,index)=>(
-                                <option key={index} value={category.categoryName}>{category.categoryName}</option>
-                            )):categoryList.filter(category=>category.type==="income").map((category,index)=>(
-                                <option key={index} value={category.categoryName}>{category.categoryName}</option>
-                            ))
-                           }
-                        </select>
-                    </label>
-                    {errors.category ? <span className="text-danger">{errors.category.message}</span> : null}
-
+                <div className={styles.transaction__bottom}>
+                <input 
+                        type="submit" 
+                        value="Update" 
+                        className={styles.transaction__button} />
                 </div>
-                <div className="form-check">
-                    <label>Date</label>
-                    <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
-                    {errors.date ? <span className="text-danger">{errors.date.message}</span> : null}
-                </div>
-
-                <input type="submit" value="Update" className="btn btn-success" />
             </form>
-            </div>
+
             
         </div>
+        <RightMenu userId={userId}></RightMenu>
+        </>
     )
 }
 
